7437cae22fa8b02441f5bcb5c1a4da8fdbc7ef7d
Abhishek <abishekmeenameena086gmail.com>
2026-04-15 22:27:57 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const ALLOWED_ORIGINS = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
-  'https://studenthub-psi.vercel.app',
+  'https://studenthub2.vercel.app', 
 ];
 
 app.use((req, res, next) => {
